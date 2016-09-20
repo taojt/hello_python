5b485daaa24b12c785f419101d0dd0541a7cc02b
Jang Tao <jt1310167566@hotmail.com>
2016-09-20 20:32:39 +0800

    add do_listcompr file

--- do_listcompr.py
+++ do_listcompr.py
@@ -14,3 +14,9 @@ print([d for d in os.listdir('.')])
 d = {'x': 1, 'y': 23, 'z': "hello"}
 for k, v in d.items():
     print(k, '=', v)
+
+# test, 将list 中的字符变量变成小写形式，生成新列表
+# isinstance(x,y) 判断x是否是y类型的对象
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
+print(L2)
