6e0887e69d44fbf4fddcd6bfe733e675c5391b11
Jang Tao <jt1310167566@hotmail.com>
2016-09-21 16:37:41 +0800

    use_oop file add setter

--- python_oop.py
+++ python_oop.py
@@ -14,6 +14,15 @@ class Student(object):
     def get_score(self):
         return self.__score
 
+    def set_name(self, name):
+        self.__name = name
+
+    def set_score(self, score):
+        if score >= 0 and score<= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad eeror.')
+
 
 if __name__ == '__main__':
     bart = Student('Bart Simpson', 86)
@@ -23,5 +32,12 @@ if __name__ == '__main__':
     # 访问内部私有变量出错
     # print(bart.__score)
 
+    # getter
     print(bart.get_name(), bart.get_score())
+
+    # setter
+    bart.set_name("Tom")
+    bart.set_score(89)
+    print(bart.get_name(), bart.get_score())
+
 
